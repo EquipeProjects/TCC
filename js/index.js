55d14e91f05a9c908cea858bec0f66ef87ff052b
João Silva <joaosilvavic2017@gmail.com>
2023-06-17 13:43:08 -0300

    at1..1.3
    melhoria do carrosel

--- js/index.js
+++ js/index.js
@@ -15,3 +15,19 @@ function nextImage() {
     document.getElementById("radio"+count).checked = true;
 
 }
+const carrossel = document.querySelector('.carrossel');
+const slides = carrossel.querySelector('ul');
+const slideWidth = carrossel.offsetWidth;
+const acaoAnterior = carrossel.querySelector('.acao-anterior');
+const acaoProxima = carrossel.querySelector('.acao-proxima');
+let posicaoAtual = 0;
+
+acaoAnterior.addEventListener('click', () => {
+  posicaoAtual = Math.max(posicaoAtual - slideWidth, -slideWidth * (slides.children.length - 1));
+  slides.style.left = `${posicaoAtual}px`;
+});
+
+acaoProxima.addEventListener('click', () => {
+  posicaoAtual = Math.min(posicaoAtual + slideWidth, 0);
+  slides.style.left = `${posicaoAtual}px`;
+});
