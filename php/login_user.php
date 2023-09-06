feae4bdc0595c3f5d6c3cb178101fff3c24a6559
João Silva <joaosilvavic2017@gmail.com>
2023-09-05 21:07:33 -0300

    login_uptade

--- php/login_user.php
+++ php/login_user.php
@@ -13,24 +13,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Recebe dados do formulário
     $username = $_POST["username"];
     $subname = $_POST["subname"];
-    $email = $_POST["email"];
-    $phone = $_POST["phone"];
+    $emailOrPhone = $_POST["emailOrPhone"]; // Alterei o nome do campo para "emailOrPhone"
     $password = $_POST["password"];
     $reppassword = $_POST["reppassword"];
 
+    // Verifica se o campo "emailOrPhone" é um email válido ou um número de telefone válido
+    if (filter_var($emailOrPhone, FILTER_VALIDATE_EMAIL)) {
+        // O campo "emailOrPhone" é um email válido
+        $sql = "SELECT id, username, subname, email, phone, password FROM usuarios WHERE email = ?";
+        $field = "email";
+    } elseif (preg_match("/^[0-9]{10}$/", $emailOrPhone)) {
+        // O campo "emailOrPhone" é um número de telefone válido (10 dígitos)
+        $sql = "SELECT id, username, subname, email, phone, password FROM usuarios WHERE phone = ?";
+        $field = "phone";
+    } else {
+        // O campo "emailOrPhone" não é um email válido nem um número de telefone válido
+        echo "Email ou telefone inválido.";
+        exit; // Saia do script, pois não é possível continuar com dados inválidos.
+    }
+
     // Consulta o banco de dados para encontrar o usuário
-    $sql = "SELECT id, username,subname,email,phone,  password FROM usuarios WHERE username = ?";
     $stmt = $mysqli->prepare($sql);
-    $stmt->bind_param("s", $username);
+    $stmt->bind_param("s", $emailOrPhone);
     $stmt->execute();
     $stmt->store_result();
 
     if ($stmt->num_rows == 1) {
-        $stmt->bind_result($id, $username, $hashed_password);
+        $stmt->bind_result($id, $username, $subname, $email, $phone, $hashed_password);
         if ($stmt->fetch() && password_verify($password, $hashed_password)) {
-            $_SESSION["id"] = $id;
-            $_SESSION["username"] = $username;
-            header("Location: dashboard.php"); // Redirecionar para a página de dashboard
+            // Verifica se as senhas são iguais
+            if ($password == $reppassword) {
+                $_SESSION["id"] = $id;
+                $_SESSION["username"] = $username;
+                header("Location: dashboard.php"); // Redirecionar para a página de dashboard
+            } else {
+                echo "As senhas não coincidem.";
+            }
         } else {
             echo "Senha incorreta.";
         }
@@ -54,17 +72,17 @@ $mysqli->close();
         <label for="username">Nome:</label>
         <input type="text" name="username" required><br>
 
-        <label for="username">Sobrenome:</label>
-        <input type="text" name="username" required><br>
+        <label for="subname">Sobrenome:</label>
+        <input type="text" name="subname" required><br>
 
-        <label for="username">Email ou telefone:</label>
-        <input type="text" name="username" required><br>
+        <label for="emailOrPhone">Email ou telefone:</label>
+        <input type="text" name="emailOrPhone" required><br>
 
         <label for="password">Senha:</label>
         <input type="password" name="password" required><br>
 
-        <label for="password">Repita a senha:</label>
-        <input type="password" name="password" required><br>
+        <label for="reppassword">Repita a senha:</label>
+        <input type="password" name="reppassword" required><br>
         
 
         <input type="submit" value="Entrar">
