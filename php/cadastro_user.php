4adcc8507b7599e84d944e73685bbab69f42734f
João Silva <joaosilvavic2017@gmail.com>
2023-09-14 20:26:38 -0300

    eee

--- php/cadastro_user.php
+++ php/cadastro_user.php
@@ -15,43 +15,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Verifica se um valor foi fornecido para o campo email ou telefone
     if (!empty($email_or_phone)) {
-        // Validação do email (caso seja um email)
+        // Verifica se é um email válido
         if (filter_var($email_or_phone, FILTER_VALIDATE_EMAIL)) {
-            // Inserção dos dados na tabela 'usuarios' com email
-            $sql = "INSERT INTO usuarios (username, password, email) VALUES (?, ?, ?)";
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("sss", $username, $password, $email_or_phone);
-
-            if ($stmt->execute()) {
-                // Redireciona o usuário para uma página externa após o cadastro bem-sucedido
-                header("Location: dashboard_user.php");
-                exit();
-            } else {
-                echo "Erro ao cadastrar: " . $stmt->error;
-            }
-
-            $stmt->close();
-        } else {
-            // Inserção dos dados na tabela 'usuarios' com telefone (caso não seja um email)
             $sql = "INSERT INTO usuarios (username, password, phone) VALUES (?, ?, ?)";
-            $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("sss", $username, $password, $email_or_phone);
-
-            if ($stmt->execute()) {
-                // Redireciona o usuário para uma página externa após o cadastro bem-sucedido
-                header("Location: dashboard_user.php");
-                exit();
-            } else {
-                echo "Erro ao cadastrar: " . $stmt->error;
-            }
-
-            $stmt->close();
+        } else {
+            $sql = "INSERT INTO usuarios (username, password, email) VALUES (?, ?, ?)";
         }
+
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param("sss", $username, $password, $email_or_phone);
+
+        if ($stmt->execute()) {
+            // Redireciona o usuário para uma página externa após o cadastro bem-sucedido
+            header("Location: dashboard_user.php");
+            exit();
+        } else {
+            echo "Erro ao cadastrar: " . $stmt->error;
+        }
+
+        $stmt->close();
     } else {
         echo "Você deve fornecer um email ou um telefone.";
     }
 }
-
 $mysqli->close();
 ?>
 
@@ -63,16 +49,17 @@ $mysqli->close();
 <body>
     <h2>Cadastro</h2>
     <form method="post" action="cadastro_user.php">
-        <label for="username">Nome de Usuário:</label>
-        <input type="text" name="username" required><br>
+        <p>CRIAR CONTA</p>
+       
+        <input type="text" name="username"  placeholder="NOME E SOBRENOME"><br>
 
-        <label for="password">Senha:</label>
-        <input type="password" name="password" required><br>
+        
+        <input type="text" name="email_or_phone" placeholder="Email ou Telefone" required><br>
 
-        <label for="email_or_phone">Email ou Telefone:</label>
-        <input type="text" name="email_or_phone" required><br>
+       
+        <input type="password" name="password" placeholder="Senha" required><br>
 
-        <input type="submit" value="Cadastrar1">
+        <input type="submit" value="Cadastrar">
     </form>
 </body>
 </html>
