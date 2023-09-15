57abc2b6cf32a5223a343b5aa9aee787f544c780
Davi Ribeiro André <Daviribeiroandre67@gmail.com>
2023-09-15 06:46:00 -0300

    tt

--- admin/visualisar_produtos.php
+++ admin/visualisar_produtos.php
@@ -60,7 +60,7 @@
                 
                     
                     ?></td>
-                    <td><a href='cadastro.php?edit_products=<?php echo $product_id; ?>' class='text-light'><i class='fa-solid fa-pen-to-square'></i>Update</a></td>
+                    <td><a href='editar_produto.php=<?php echo $product_id; ?>' class='text-light'><i class='fa-solid fa-pen-to-square'></i>Update</a></td>
                     <td><a href='index.php?delete_product=<?php echo $product_id; ?>' class='text-light'><i class='fa-solid fa-trash'></i>Delete</a></td>
                 </tr>
                 <?php
