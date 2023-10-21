a47da129d2f2fcfa99626092de8cb90f4e674070
João Silva <joaosilvavic2017@gmail.com>
2023-10-20 21:39:54 -0300

    uuu

--- politica_privacid.php
+++ politica_privacid.php
@@ -11,8 +11,7 @@
     <link rel="shortcut icon" href="/ico/logo.ico" type="image/x-icon">
     <meta name="author" content="João Victor,Davi Ribeiro e Yzabella Luiza">
     <meta name="keywords" content="HTML,CSS,JavaScript">
-    <meta name="description"
-        content="Um web site de vendas de roupas sob medida que adequa qualquer corpo,gosto e estilo.">
+    <meta name="description" content="Um web site de vendas de roupas sob medida que adequa qualquer corpo,gosto e estilo.">
 </head>
 
 <body>
@@ -20,133 +19,74 @@
         <?php
         include('php/header.php'); // Inclui o cabeçalho
         ?>
-       
 
-        
+
+
         </div>
     </header>
 
     <main>
         <div class="ajuda_menu">
             <div class="title_ajuda">Política de Privacidade </div>
-            <div class="text_pedidos">
-                <div>Meus pedidos</div>
-                <a class="links_pedidos" href="#">
-                    <div>Acompanhar meus pedidos</div>
-                    <img class="seta_pedido" src="img/seta_black.png">
-                </a>
-                <a class="links_pedidos" href="#">
-                    <div>Acessar ou estornar meus pedidos</div>
-                    <img class="seta_pedido" src="img/seta_black.png">
-                </a>
-                <a class="links_pedidos" href="#">
-                    <div>Verificar status de pagamento</div>
-                    <img class="seta_pedido" src="img/seta_black.png">
-                </a>
+
+            <div class="backgroun_privacidade">
+                Data de Entrada em Vigor: [Inserir data]
+
+                A EasyFit, adiante referida como "nós", "nosso" ou "nossa", está comprometida em proteger a privacidade dos nossos clientes. Esta política de privacidade descreve como coletamos, usamos, compartilhamos e protegemos as informações pessoais que você fornece quando utiliza o nosso site de comércio eletrônico.
+
+                1. Informações Coletadas
+
+                1.1. Informações de Cadastro: Quando você cria uma conta em nosso site, coletamos informações pessoais, incluindo seu nome, endereço de e-mail, endereço de entrega e outras informações necessárias para processar o seu pedido e fornecer suporte ao cliente.
+
+                1.2. Informações de Pagamento: Ao efetuar uma compra, coletamos informações de pagamento, como números de cartão de crédito ou débito, informações de faturamento e outros detalhes relevantes para processar transações.
+
+                1.3. Informações de Navegação: Coletamos informações sobre como você utiliza o nosso site, incluindo o seu endereço IP, tipo de navegador, sistema operacional, páginas visitadas e datas e horários de acesso.
+
+                2. Uso das Informações
+
+                2.1. Processamento de Pedidos: Utilizamos as informações pessoais fornecidas para processar os pedidos, entregar produtos e fornecer serviços de suporte ao cliente.
+
+                2.2. Comunicações: Podemos usar seu endereço de e-mail para enviar informações sobre seus pedidos, promoções, atualizações e newsletters. Você pode optar por não receber comunicações de marketing a qualquer momento.
+
+                2.3. Melhoria do Site: As informações de navegação são utilizadas para melhorar a experiência do usuário, personalizar o conteúdo do site e analisar o desempenho do site.
+
+                3. Compartilhamento de Informações
+
+                3.1. Terceiros: Podemos compartilhar informações pessoais com terceiros que nos ajudam a fornecer serviços, como processamento de pagamentos, entrega de produtos e análise de dados. Esses terceiros têm políticas de privacidade próprias.
+
+                4. Segurança de Dados
+
+                Implementamos medidas de segurança para proteger suas informações pessoais contra acesso não autorizado, uso indevido ou divulgação.
+
+                5. Seus Direitos
+
+                Você tem o direito de acessar, corrigir ou excluir suas informações pessoais. Você também pode optar por não receber comunicações de marketing. Para exercer esses direitos ou para fazer perguntas sobre esta política de privacidade, entre em contato conosco através das informações de contato fornecidas no final deste documento.
+
+                6. Alterações na Política de Privacidade
+
+                Reservamo-nos o direito de atualizar esta política de privacidade conforme necessário. Quaisquer alterações serão publicadas em nosso site e entrarão em vigor na data da publicação.
+
+                7. Contato
+
+                Para entrar em contato conosco com perguntas ou preocupações sobre esta política de privacidade, envie um e-mail para [Inserir endereço de e-mail de contato] ou escreva para o endereço [Inserir endereço físico, se aplicável].
+
+                Esta política de privacidade destina-se a informá-lo sobre como tratamos suas informações pessoais. Ao utilizar nosso site, você concorda com as práticas descritas nesta política.
             </div>
-            <div class="text_cancelamentos">
-                <div>Troca e Cancelamento</div>
-                <a class="links_cancelamento" href="#">
-                    <div>Solicitar troca ou devolução</div>
-                    <img class="seta_cancela" src="img/seta_black.png">
-                </a>
-                <a class="links_cancelamento" href="#">
-                    <div>Cancelar um pedido ou produto</div>
-                    <img class="seta_cancela" src="img/seta_black.png">
-                </a>
-            </div>
-            <div class="text_cadastro">
-                <div>Meu Cadastro</div>
-                <a class="links_cadastro" href="#">
-                    <div>Alterar meus dados</div>
-                    <img class="seta_cadast" src="img/seta_black.png">
-                </a>
-                <a class="links_cadastro" href="#">
-                    <div>Alterar meu endereço</div>
-                    <img class="seta_cadast" src="img/seta_black.png">
-                </a>
-                <a class="links_cadastro" href="#">
-                    <div>Alterar minha senha</div>
-                    <img class="seta_cadast" src="img/seta_black.png">
-                </a>
-            </div>
-            <div class="text_duvidas">DÚVIDAS FREQUENTES</div>
 
-            <a class="link_duvida" href="#">
-                <div>TENHA ACESSO AO STATUS DO SEU PEDIDO ATRAVÉS DE SUA CONTA</div>
-            </a>
-            <a class="link_duvida" href="#">
-                <div> CONHEÇA NOSSA POLÍTICA, TIPOS E PRAZOS DE ENTREGA E FRETE</div>
-            </a>
-            <a class="link_duvida" href="#"> 
-                <div>SAIBA COMO TROCAR OU DEVOLVER UM PRODUTO E TODAS AS CONDIÇÕES</div>
-            </a>
-            <a class="link_duvida" href="#">
-                <div>SAIBA QUAIS MEIOS DE PAGAMENTO SÃO ACEITOS E TODAS AS CONDIÇÕES</div>
-            </a>
-            <a class="link_duvida" href="#">
-                <div>INFORMAÇÃO DE COMO UTILIZAR VOUCHER DE DESCONTO</div>
-            </a>
-            <div class="title_contato">Entre em contato</div>
-            <div class="title_email">E-MAIL</div>
-            <img class="img_email" src="img/email.png">
-            <div class="title_fone">TELEFONE</div>
-            <img class="img_fone" src="img/telefone.png">
-            <div class="text_email">EasyFiTcorporation@gmail.com</div>
-            <div class="text_fone">(12)9005-8876</div>
-            Data de Entrada em Vigor: [Inserir data]
 
-A EasyFit, adiante referida como "nós", "nosso" ou "nossa", está comprometida em proteger a privacidade dos nossos clientes. Esta política de privacidade descreve como coletamos, usamos, compartilhamos e protegemos as informações pessoais que você fornece quando utiliza o nosso site de comércio eletrônico.
 
-1. Informações Coletadas
-
-1.1. Informações de Cadastro: Quando você cria uma conta em nosso site, coletamos informações pessoais, incluindo seu nome, endereço de e-mail, endereço de entrega e outras informações necessárias para processar o seu pedido e fornecer suporte ao cliente.
-
-1.2. Informações de Pagamento: Ao efetuar uma compra, coletamos informações de pagamento, como números de cartão de crédito ou débito, informações de faturamento e outros detalhes relevantes para processar transações.
-
-1.3. Informações de Navegação: Coletamos informações sobre como você utiliza o nosso site, incluindo o seu endereço IP, tipo de navegador, sistema operacional, páginas visitadas e datas e horários de acesso.
-
-2. Uso das Informações
-
-2.1. Processamento de Pedidos: Utilizamos as informações pessoais fornecidas para processar os pedidos, entregar produtos e fornecer serviços de suporte ao cliente.
-
-2.2. Comunicações: Podemos usar seu endereço de e-mail para enviar informações sobre seus pedidos, promoções, atualizações e newsletters. Você pode optar por não receber comunicações de marketing a qualquer momento.
-
-2.3. Melhoria do Site: As informações de navegação são utilizadas para melhorar a experiência do usuário, personalizar o conteúdo do site e analisar o desempenho do site.
-
-3. Compartilhamento de Informações
-
-3.1. Terceiros: Podemos compartilhar informações pessoais com terceiros que nos ajudam a fornecer serviços, como processamento de pagamentos, entrega de produtos e análise de dados. Esses terceiros têm políticas de privacidade próprias.
-
-4. Segurança de Dados
-
-Implementamos medidas de segurança para proteger suas informações pessoais contra acesso não autorizado, uso indevido ou divulgação.
-
-5. Seus Direitos
-
-Você tem o direito de acessar, corrigir ou excluir suas informações pessoais. Você também pode optar por não receber comunicações de marketing. Para exercer esses direitos ou para fazer perguntas sobre esta política de privacidade, entre em contato conosco através das informações de contato fornecidas no final deste documento.
-
-6. Alterações na Política de Privacidade
-
-Reservamo-nos o direito de atualizar esta política de privacidade conforme necessário. Quaisquer alterações serão publicadas em nosso site e entrarão em vigor na data da publicação.
-
-7. Contato
-
-Para entrar em contato conosco com perguntas ou preocupações sobre esta política de privacidade, envie um e-mail para [Inserir endereço de e-mail de contato] ou escreva para o endereço [Inserir endereço físico, se aplicável].
-
-Esta política de privacidade destina-se a informá-lo sobre como tratamos suas informações pessoais. Ao utilizar nosso site, você concorda com as práticas descritas nesta política.
     </main>
-    <?php 
+    <?php
     include('php/footer.php')
     ?>
     <script src="js/index.js"></script>
 
     <script>
-        document.addEventListener('DOMContentLoaded', function () {
+        document.addEventListener('DOMContentLoaded', function() {
             const hamburgerMenu = document.querySelector('.hamburger-menu');
             const centerLinks = document.querySelector('.center-links ul');
-    
-            hamburgerMenu.addEventListener('click', function () {
+
+            hamburgerMenu.addEventListener('click', function() {
                 centerLinks.classList.toggle('active');
             });
         });
